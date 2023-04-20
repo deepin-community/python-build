27847890bb106359158d7dc6452572ed69c5adc0
hudeng <hudeng@deepin.org>
2023-04-20 12:20:27 +0800

    feat: update new version
    fix for dh-python runtime depends

--- src/build/env.py
+++ src/build/env.py
@@ -2,6 +2,8 @@
 Creates and manages isolated build environments.
 """
 import abc
+import functools
+import logging
 import os
 import platform
 import shutil
@@ -9,207 +11,330 @@ import subprocess
 import sys
 import sysconfig
 import tempfile
-from types import TracebackType
-from typing import Iterable, Optional, Tuple, Type
+import warnings
 
-from ._compat import abstractproperty, add_metaclass
+from types import TracebackType
+from typing import Callable, Collection, List, Optional, Tuple, Type
+
+import build
+
 
 try:
-    import pip
-except ImportError:  # pragma: no cover
-    pip = None  # pragma: no cover
+    import virtualenv
+except ModuleNotFoundError:
+    virtualenv = None
 
 
-@add_metaclass(abc.ABCMeta)
-class IsolatedEnv(object):
+_logger = logging.getLogger(__name__)
+
+
+class IsolatedEnv(metaclass=abc.ABCMeta):
     """Abstract base of isolated build environments, as required by the build project."""
 
-    @abstractproperty
-    def executable(self):  # type: () -> str
-        """Return the executable of the isolated build environment."""
+    @property
+    @abc.abstractmethod
+    def executable(self) -> str:
+        """The executable of the isolated build environment."""
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def scripts_dir(self) -> str:
+        """The scripts directory of the isolated build environment."""
         raise NotImplementedError
 
     @abc.abstractmethod
-    def install(self, requirements):  # type: (Iterable[str]) -> None
+    def install(self, requirements: Collection[str]) -> None:
         """
-        Install PEP-508 requirements into the isolated build environment.
+        Install packages from PEP 508 requirements in the isolated build environment.
 
-        :param requirements: PEP-508 requirements
+        :param requirements: PEP 508 requirements
         """
         raise NotImplementedError
 
 
-class IsolatedEnvBuilder(object):
-    def __init__(self):  # type: () -> None
-        """Builder object for isolated environment."""
-        self._path = None  # type: Optional[str]
+@functools.lru_cache(maxsize=None)
+def _should_use_virtualenv() -> bool:
+    import packaging.requirements
 
-    def __enter__(self):  # type: () -> IsolatedEnv
-        """
-        Creates an isolated build environment.
+    # virtualenv might be incompatible if it was installed separately
+    # from build. This verifies that virtualenv and all of its
+    # dependencies are installed as specified by build.
+    return virtualenv is not None and not any(
+        packaging.requirements.Requirement(d[1]).name == 'virtualenv'
+        for d in build.check_dependency('build[virtualenv]')
+        if len(d) > 1
+    )
 
-        :return: the isolated build environment
+
+def _subprocess(cmd: List[str]) -> None:
+    """Invoke subprocess and output stdout and stderr if it fails."""
+    try:
+        subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(e.output.decode(), end='', file=sys.stderr)
+        raise e
+
+
+class IsolatedEnvBuilder:
+    """Builder object for isolated environments."""
+
+    def __init__(self) -> None:
+        self._path: Optional[str] = None
+
+    def __enter__(self) -> IsolatedEnv:
         """
-        self._path = tempfile.mkdtemp(prefix='build-env-')
+        Create an isolated build environment.
+
+        :return: The isolated build environment
+        """
+        # Call ``realpath`` to prevent spurious warning from being emitted
+        # that the venv location has changed on Windows. The username is
+        # DOS-encoded in the output of tempfile - the location is the same
+        # but the representation of it is different, which confuses venv.
+        # Ref: https://bugs.python.org/issue46171
+        self._path = os.path.realpath(tempfile.mkdtemp(prefix='build-env-'))
         try:
-            executable, pip_executable = _create_isolated_env(self._path)
-            return _IsolatedEnvVenvPip(path=self._path, python_executable=executable, pip_executable=pip_executable)
+            # use virtualenv when available (as it's faster than venv)
+            if _should_use_virtualenv():
+                self.log('Creating virtualenv isolated environment...')
+                executable, scripts_dir = _create_isolated_env_virtualenv(self._path)
+            else:
+                self.log('Creating venv isolated environment...')
+                executable, scripts_dir = _create_isolated_env_venv(self._path)
+            return _IsolatedEnvVenvPip(
+                path=self._path,
+                python_executable=executable,
+                scripts_dir=scripts_dir,
+                log=self.log,
+            )
         except Exception:  # cleanup folder if creation fails
             self.__exit__(*sys.exc_info())
             raise
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # type: (Optional[Type[BaseException]], Optional[BaseException], Optional[TracebackType]) -> None
+    def __exit__(
+        self, exc_type: Optional[Type[BaseException]], exc_val: Optional[BaseException], exc_tb: Optional[TracebackType]
+    ) -> None:
         """
         Delete the created isolated build environment.
 
-        :param exc_type: the type of exception raised (if any)
-        :param exc_val: the value of exception raised (if any)
-        :param exc_tb: the traceback of exception raised (if any)
+        :param exc_type: The type of exception raised (if any)
+        :param exc_val: The value of exception raised (if any)
+        :param exc_tb: The traceback of exception raised (if any)
         """
         if self._path is not None and os.path.exists(self._path):  # in case the user already deleted skip remove
             shutil.rmtree(self._path)
+
+    @staticmethod
+    def log(message: str) -> None:
+        """
+        Prints message
+
+        The default implementation uses the logging module but this function can be
+        overwritten by users to have a different implementation.
+
+        :param msg: Message to output
+        """
+        if sys.version_info >= (3, 8):
+            _logger.log(logging.INFO, message, stacklevel=2)
+        else:
+            _logger.log(logging.INFO, message)
 
 
 class _IsolatedEnvVenvPip(IsolatedEnv):
     """
     Isolated build environment context manager
 
-    Non-standard paths injected directly to sys.path still be passed to the environment.
+    Non-standard paths injected directly to sys.path will still be passed to the environment.
     """
 
-    def __init__(self, path, python_executable, pip_executable):
-        # type: (str, str, str) -> None
+    def __init__(
+        self,
+        path: str,
+        python_executable: str,
+        scripts_dir: str,
+        log: Callable[[str], None],
+    ) -> None:
         """
-        Define an isolated build environment.
-
-        :param path: the path where the environment exists
-        :param python_executable: the python executable within the environment
-        :param pip_executable: an executable that allows installing packages within the environment
+        :param path: The path where the environment exists
+        :param python_executable: The python executable within the environment
+        :param log: Log function
         """
         self._path = path
-        self._pip_executable = pip_executable
         self._python_executable = python_executable
+        self._scripts_dir = scripts_dir
+        self._log = log
 
     @property
-    def path(self):  # type: () -> str
-        """:return: the location of the isolated build environment"""
+    def path(self) -> str:
+        """The location of the isolated build environment."""
         return self._path
 
     @property
-    def executable(self):  # type: () -> str
-        """:return: the python executable of the isolated build environment"""
+    def executable(self) -> str:
+        """The python executable of the isolated build environment."""
         return self._python_executable
 
-    def install(self, requirements):  # type: (Iterable[str]) -> None
+    @property
+    def scripts_dir(self) -> str:
+        return self._scripts_dir
+
+    def install(self, requirements: Collection[str]) -> None:
         """
-        Installs the specified PEP 508 requirements on the environment
+        Install packages from PEP 508 requirements in the isolated build environment.
 
-        :param requirements: PEP-508 requirement specification to install
+        :param requirements: PEP 508 requirement specification to install
 
-        :note: Passing non PEP 508 strings will result in undefined behavior, you *should not* rely on it. It is \
+        :note: Passing non-PEP 508 strings will result in undefined behavior, you *should not* rely on it. It is
                merely an implementation detail, it may change any time without warning.
         """
         if not requirements:
             return
 
+        self._log('Installing packages in isolated environment... ({})'.format(', '.join(sorted(requirements))))
+
+        # pip does not honour environment markers in command line arguments
+        # but it does for requirements from a file
         with tempfile.NamedTemporaryFile('w+', prefix='build-reqs-', suffix='.txt', delete=False) as req_file:
             req_file.write(os.linesep.join(requirements))
         try:
             cmd = [
-                self._pip_executable,
-                # on python2 if isolation is achieved via environment variables, we need to ignore those while calling
-                # host python (otherwise pip would not be available within it)
-                '-{}m'.format('E' if self._pip_executable == self.executable and sys.version_info[0] == 2 else ''),
+                self.executable,
+                '-Im',
                 'pip',
                 'install',
-                '--prefix',
-                self.path,
-                '--ignore-installed',
+                '--use-pep517',
                 '--no-warn-script-location',
                 '-r',
                 os.path.abspath(req_file.name),
             ]
-            subprocess.check_call(cmd)
+            _subprocess(cmd)
         finally:
             os.unlink(req_file.name)
 
 
-if sys.version_info[0] == 2:  # noqa: C901 # disable if too complex
+def _create_isolated_env_virtualenv(path: str) -> Tuple[str, str]:
+    """
+    We optionally can use the virtualenv package to provision a virtual environment.
 
-    def _create_isolated_env(path):  # type: (str) -> Tuple[str, str]
-        """
-        On Python 2 we use the virtualenv package to provision a virtual environment.
-
-        :param path: the folder where to create the isolated build environment
-        :return: the isolated build environment executable, and the pip to use to install packages into it
-        """
-        from virtualenv import cli_run
-
-        cmd = [str(path), '--no-setuptools', '--no-wheel', '--activators', '']
-        if pip is not None:
-            cmd.append('--no-pip')
-        result = cli_run(cmd, setup_logging=False)
-        executable = str(result.creator.exe)
-        pip_executable = executable if pip is None else sys.executable
-        return executable, pip_executable
+    :param path: The path where to create the isolated build environment
+    :return: The Python executable and script folder
+    """
+    cmd = [str(path), '--no-setuptools', '--no-wheel', '--activators', '']
+    result = virtualenv.cli_run(cmd, setup_logging=False)
+    executable = str(result.creator.exe)
+    script_dir = str(result.creator.script_dir)
+    return executable, script_dir
 
 
-else:
+@functools.lru_cache(maxsize=None)
+def _fs_supports_symlink() -> bool:
+    """Return True if symlinks are supported"""
+    # Using definition used by venv.main()
+    if not sys.platform.startswith('win'):
+        return True
 
-    def _create_isolated_env(path):  # type: (str) -> Tuple[str, str]
-        """
-        On Python 3 we use the venv package from the standard library, and if host python has no pip the ensurepip
-        package to provision one into the created virtual environment.
-
-        :param path: the folder where to create the isolated build environment
-        :return: the isolated build environment executable, and the pip to use to install packages into it
-        """
-        import venv
-
-        venv.EnvBuilder(with_pip=False).create(path)
-        executable = _find_executable(path)
-
-        # Scenario 1: pip is available (either installed or via pth file) within the python executable alongside
-        # this projects environment: in this case we should be able to import it
-        if pip is not None:
-            pip_executable = sys.executable
-        else:
-            # Scenario 2: this project is installed into a virtual environment that has no pip, but the system has
-            # Scenario 3: there's a pip executable on PATH
-            # Scenario 4: no pip can be found, we might be able to provision one into the build env via ensurepip
-            cmd = [executable, '-Im', 'ensurepip', '--upgrade', '--default-pip']
-            try:
-                subprocess.check_call(cmd, cwd=path)
-            except subprocess.CalledProcessError:  # pragma: no cover
-                pass  # pragma: no cover
-            # avoid the setuptools from ensurepip to break the isolation
-            subprocess.check_call([executable, '-Im', 'pip', 'uninstall', 'setuptools', '-y'])
-            pip_executable = executable
-        return executable, pip_executable
-
-    def _find_executable(path):  # type: (str) -> str
-        """
-        Detect the executable within a virtual environment.
-
-        :param path: the location of the virtual environment
-        :return: the python executable
-        """
-        config_vars = sysconfig.get_config_vars().copy()  # globally cached, copy before altering it
-        config_vars['base'] = path
-        env_scripts = sysconfig.get_path('scripts', vars=config_vars)
-        if not env_scripts:
-            raise RuntimeError("Couldn't get environment scripts path")
-        exe = 'pypy3' if platform.python_implementation() == 'PyPy' else 'python'
-        if os.name == 'nt':
-            exe = '{}.exe'.format(exe)
-        executable = os.path.join(path, env_scripts, exe)
-        if not os.path.exists(executable):
-            raise RuntimeError('Virtual environment creation failed, executable {} missing'.format(executable))
-        return executable
+    # Windows may support symlinks (setting in Windows 10)
+    with tempfile.NamedTemporaryFile(prefix='build-symlink-') as tmp_file:
+        dest = f'{tmp_file}-b'
+        try:
+            os.symlink(tmp_file.name, dest)
+            os.unlink(dest)
+            return True
+        except (OSError, NotImplementedError, AttributeError):
+            return False
 
 
-__all__ = (
+def _create_isolated_env_venv(path: str) -> Tuple[str, str]:
+    """
+    On Python 3 we use the venv package from the standard library.
+
+    :param path: The path where to create the isolated build environment
+    :return: The Python executable and script folder
+    """
+    import venv
+
+    import packaging.version
+
+    if sys.version_info < (3, 8):
+        import importlib_metadata as metadata
+    else:
+        from importlib import metadata
+
+    symlinks = _fs_supports_symlink()
+    try:
+        with warnings.catch_warnings():
+            if sys.version_info[:3] == (3, 11, 0):
+                warnings.filterwarnings('ignore', 'check_home argument is deprecated and ignored.', DeprecationWarning)
+            venv.EnvBuilder(with_pip=True, symlinks=symlinks).create(path)
+    except subprocess.CalledProcessError as exc:
+        raise build.FailedProcessError(exc, 'Failed to create venv. Maybe try installing virtualenv.') from None
+
+    executable, script_dir, purelib = _find_executable_and_scripts(path)
+
+    # Get the version of pip in the environment
+    pip_distribution = next(iter(metadata.distributions(name='pip', path=[purelib])))  # type: ignore[no-untyped-call]
+    current_pip_version = packaging.version.Version(pip_distribution.version)
+
+    if platform.system() == 'Darwin' and int(platform.mac_ver()[0].split('.')[0]) >= 11:
+        # macOS 11+ name scheme change requires 20.3. Intel macOS 11.0 can be told to report 10.16 for backwards
+        # compatibility; but that also fixes earlier versions of pip so this is only needed for 11+.
+        is_apple_silicon_python = platform.machine() != 'x86_64'
+        minimum_pip_version = '21.0.1' if is_apple_silicon_python else '20.3.0'
+    else:
+        # PEP-517 and manylinux1 was first implemented in 19.1
+        minimum_pip_version = '19.1.0'
+
+    if current_pip_version < packaging.version.Version(minimum_pip_version):
+        _subprocess([executable, '-m', 'pip', 'install', f'pip>={minimum_pip_version}'])
+
+    # Avoid the setuptools from ensurepip to break the isolation
+    _subprocess([executable, '-m', 'pip', 'uninstall', 'setuptools', '-y'])
+    return executable, script_dir
+
+
+def _find_executable_and_scripts(path: str) -> Tuple[str, str, str]:
+    """
+    Detect the Python executable and script folder of a virtual environment.
+
+    :param path: The location of the virtual environment
+    :return: The Python executable, script folder, and purelib folder
+    """
+    config_vars = sysconfig.get_config_vars().copy()  # globally cached, copy before altering it
+    config_vars['base'] = path
+    scheme_names = sysconfig.get_scheme_names()
+    if 'venv' in scheme_names:
+        # Python distributors with custom default installation scheme can set a
+        # scheme that can't be used to expand the paths in a venv.
+        # This can happen if build itself is not installed in a venv.
+        # The distributors are encouraged to set a "venv" scheme to be used for this.
+        # See https://bugs.python.org/issue45413
+        # and https://github.com/pypa/virtualenv/issues/2208
+        paths = sysconfig.get_paths(scheme='venv', vars=config_vars)
+    elif 'posix_local' in scheme_names:
+        # The Python that ships on Debian/Ubuntu varies the default scheme to
+        # install to /usr/local
+        # But it does not (yet) set the "venv" scheme.
+        # If we're the Debian "posix_local" scheme is available, but "venv"
+        # is not, we use "posix_prefix" instead which is venv-compatible there.
+        paths = sysconfig.get_paths(scheme='posix_prefix', vars=config_vars)
+    elif 'osx_framework_library' in scheme_names:
+        # The Python that ships with the macOS developer tools varies the
+        # default scheme depending on whether the ``sys.prefix`` is part of a framework.
+        # But it does not (yet) set the "venv" scheme.
+        # If the Apple-custom "osx_framework_library" scheme is available but "venv"
+        # is not, we use "posix_prefix" instead which is venv-compatible there.
+        paths = sysconfig.get_paths(scheme='posix_prefix', vars=config_vars)
+    else:
+        paths = sysconfig.get_paths(vars=config_vars)
+    executable = os.path.join(paths['scripts'], 'python.exe' if sys.platform.startswith('win') else 'python')
+    if not os.path.exists(executable):
+        raise RuntimeError(f'Virtual environment creation failed, executable {executable} missing')
+
+    return executable, paths['scripts'], paths['purelib']
+
+
+__all__ = [
     'IsolatedEnvBuilder',
     'IsolatedEnv',
-)
+]
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,37 +2,15 @@
 
 import os
 import os.path
-import platform
 import shutil
 import stat
-import subprocess
 import sys
 import sysconfig
 import tempfile
 
 import pytest
 
-
-def _setup():
-    """At the start of the test suite initialize the environment in case of path/wheel/sdist mode"""
-    mode = os.environ.get('TEST_MODE')
-    root = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src')
-    if mode == 'path':
-        sys.path.insert(0, root)
-    elif mode in ('wheel', 'sdist'):
-        env = os.environ.copy()
-        env['PYTHONPATH'] = str(root)
-        temp = tempfile.mkdtemp()
-        try:
-            cmd = [sys.executable, '-m', 'build', '--{}'.format(mode), '--no-isolation', '--outdir', str(temp)]
-            subprocess.check_output(cmd, env=env)
-            pkg = next(t for t in os.listdir(temp) if (t.endswith('.whl' if mode == 'wheel' else '.tar.gz')))
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', os.path.join(temp, pkg)])
-        finally:
-            shutil.rmtree(temp)
-
-
-_setup()
+import build.env
 
 
 def pytest_addoption(parser):
@@ -41,15 +19,10 @@ def pytest_addoption(parser):
     parser.addoption('--run-integration', action='store_true', help='run the integration tests')
     parser.addoption('--only-integration', action='store_true', help='only run the integration tests')
 
-    # pytest-xdist does not work on Windows PyPy2 - we need to disable it
-    if platform.python_implementation() == 'PyPy' and sys.version_info[0] == 2 and os.name == 'nt':
-        # the patch here is compatible with pytest 4 + pytest-xdist 1.31 (last release on python2)
-        num_process_option_xdist = next(o for o in parser.getgroup('xdist').options if o.dest == 'numprocesses')
-        # change the type to return None -> no parallel runs
-        num_process_option_xdist._attrs['type'] = lambda s: None  # noqa
 
-
-PYPY3_WIN_VENV_BAD = platform.python_implementation() == 'PyPy' and sys.version_info[0] == 3 and os.name == 'nt'
+PYPY3_WIN_VENV_BAD = (
+    sys.implementation.name == 'pypy' and sys.implementation.version < (7, 3, 9) and sys.platform.startswith('win')
+)
 PYPY3_WIN_M = 'https://foss.heptapod.net/pypy/pypy/-/issues/3323 and https://foss.heptapod.net/pypy/pypy/-/issues/3321'
 
 
@@ -64,6 +37,8 @@ def pytest_collection_modifyitems(config, items):
         return
     for item in items:
         is_integration_file = is_integration(item)
+        if PYPY3_WIN_VENV_BAD and item.get_closest_marker('pypy3323bug') and os.environ.get('PYPY3323BUG', None):
+            item.add_marker(pytest.mark.xfail(reason=PYPY3_WIN_M, strict=False))
         if PYPY3_WIN_VENV_BAD and item.get_closest_marker('isolated'):
             if not (is_integration_file and item.originalname == 'test_build') or (
                 hasattr(item, 'callspec') and '--no-isolation' not in item.callspec.params.get('args', [])
@@ -93,34 +68,20 @@ def packages_path():
     return os.path.realpath(os.path.join(__file__, '..', 'packages'))
 
 
-@pytest.fixture
-def legacy_path(packages_path):
-    return os.path.join(packages_path, 'legacy')
+def generate_package_path_fixture(package_name):
+    @pytest.fixture
+    def fixture(packages_path):
+        return os.path.join(packages_path, package_name)
+
+    return fixture
 
 
-@pytest.fixture
-def test_flit_path(packages_path):
-    return os.path.join(packages_path, 'test-flit')
-
-
-@pytest.fixture
-def test_bad_syntax_path(packages_path):
-    return os.path.join(packages_path, 'test-bad-syntax')
-
-
-@pytest.fixture
-def test_no_backend_path(packages_path):
-    return os.path.join(packages_path, 'test-no-backend')
-
-
-@pytest.fixture
-def test_no_requires_path(packages_path):
-    return os.path.join(packages_path, 'test-no-requires')
-
-
-@pytest.fixture
-def test_typo(packages_path):
-    return os.path.join(packages_path, 'test-typo')
+# Generate path fixtures dynamically.
+package_names = os.listdir(os.path.join(os.path.dirname(__file__), 'packages'))
+for package_name in package_names:
+    normalized_name = package_name.replace('-', '_')
+    fixture_name = f'package_{normalized_name}'
+    globals()[fixture_name] = generate_package_path_fixture(package_name)
 
 
 @pytest.fixture
@@ -143,3 +104,8 @@ def tmp_dir():
     yield path
 
     shutil.rmtree(path)
+
+
+@pytest.fixture(autouse=True)
+def force_venv(mocker):
+    mocker.patch.object(build.env, '_should_use_virtualenv', lambda: False)
